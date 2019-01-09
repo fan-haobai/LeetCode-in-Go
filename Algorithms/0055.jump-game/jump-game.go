b5787a9a2830add262667752ad0ef0c2091e271d
fan-haobai <i@fanhaobai.com>
2019-01-09 18:54:56 +0800

    refactor: 贪心算法

--- Algorithms/0055.jump-game/jump-game.go
+++ Algorithms/0055.jump-game/jump-game.go
@@ -1,27 +1,21 @@
 package problem0055
 
 func canJump(nums []int) bool {
+	length := len(nums)
+	maxPos := 0
+	i := 0
 
-	for i := len(nums) - 2; i >= 0; i-- {
-		// 找到数值为 0 的元素
-		if nums[i] != 0 {
-			continue
-		}
-
-		j := i - 1
-		for ; j >= 0; j-- {
-			if i-j < nums[j] {
-				// 在 j 号位置上，可以跨过 0 元素
-				i = j
-				break
-			}
-		}
-
-		if j == -1 {
-			// 在 0 元素之前，没有位置可以跨过 0
-			return false
-		}
+	for ; i <= maxPos && i < length; i++ {
+		maxPos = max(maxPos, nums[i] + i);
 	}
 
-	return true
+	return i == length
+}
+
+func max(a, b int) int {
+	if (a >= b) {
+		return a;
+	}
+
+	return b;
 }
